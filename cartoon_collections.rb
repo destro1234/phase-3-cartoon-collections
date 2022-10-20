0b762ff1e26fe999e9841caae50b03b3ff383572
Elias <eliasventura23@gmail.com>
2022-10-19 20:55:26 -0400

    commit tree dfa7ba67d2d7e7bfc6d3f109c9743162589a79be

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index.map do |dwarf, index|
+    puts "#{index +1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planateer_calls)# code an argument here
   # Your code here
+  planateer_calls.map do |call|
+    "#{call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planateer_calls)# code an argument here
   # Your code here
+  planateer_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    cheese_types.include?(item)
+  end
+  # array.include?(cheese_types)
 end
